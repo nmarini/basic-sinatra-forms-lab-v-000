8f455fab93cac24437122f96201161caf0beb949
Nicolas Marini <nmarini44@gmail.com>
2018-11-17 20:25:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,13 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @name = params[:name] 
+    @name = params[:name]
     @coach = coach[:coach]
-    @pg = params[:pg] 
-    @sg = params[:sg] 
-    @sf = params[:sf] 
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
     @pf = params[:pf]
-    @c = params[:c] 
+    @c = params[:c]
 
     erb :team
   end
